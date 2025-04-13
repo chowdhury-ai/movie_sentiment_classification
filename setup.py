124b805e45f8f390b63764cf82aa3d27a876191c
chowdhury-ai <debanjanchowdhury900@gmail.com>
2025-04-13 19:06:19 +0530

    project structure made.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project demonstrates a complete end-to-end MLOps workflow using an IMDB movie reviews dataset for sentiment classification. It combines data science best practices with modern MLOps tools to streamline experimentation, model training, deployment, and monitoring in a scalable and reproducible manner.',
+    author='chowdhury-ai',
+    license='MIT',
+)
